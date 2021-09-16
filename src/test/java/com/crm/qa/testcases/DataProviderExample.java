e60523e767eabb938194e92710734c525e52a47a
KETAN KARDILE <ketankardile25@gmail.com>
2021-09-16 21:04:30 +0530

    ok
    ok

--- src/test/java/com/crm/qa/testcases/DataProviderExample.java
+++ src/test/java/com/crm/qa/testcases/DataProviderExample.java
@@ -7,12 +7,12 @@ import com.crm.qa.util.TestUtil;
 
 public class DataProviderExample {
 
-	public static TestUtil testUtil;
-
-	@Test(priority = 1)
-	public void readDataParameterization() {
-		testUtil = new TestUtil();
-	}
+//	public static TestUtil testUtil;
+//
+//	@Test(priority = 1)
+//	public void readDataParameterization() {
+//		testUtil = new TestUtil();
+//	}
 
 
 	@DataProvider
@@ -25,7 +25,11 @@ public class DataProviderExample {
 	@Test(priority = 2, dataProvider="getCRMTestData")
 	public void validateCreateNewContact(String title, String firstName, String lastName, String company){
 
-		System.out.println(title);
+		System.out.println(title + "" + firstName + "" + lastName +"" + company);
+	
+	
+	
+	
 	}
 
 }
